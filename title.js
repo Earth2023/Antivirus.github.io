0ed1c41d5dbcba6a3b379395e61886df4fd2fee7
Earth2023 <131428329+Earth2023@users.noreply.github.com>
2023-07-05 12:04:07 +0800

    Add files via upload

---
+++ title.js
@@ -0,0 +1,16 @@
+﻿const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+    const repeatedGreetings = async () => {
+      await sleep(300)
+      document.title = "Earth"
+      await sleep(300)
+      document.title = "Earth2023"
+      await sleep(300)
+      document.title = "Earth2023 Antivirus"
+      await sleep(300)
+      document.title = "Earth2023 Antivirus (Earth"
+      await sleep(300)
+      document.title = "Earth2023 Antivirus (Earth2023 "
+      await sleep(300)
+      document.title = "Earth2023 Antivirus (Earth2023 杀毒软件)"
+    }
+    repeatedGreetings()
